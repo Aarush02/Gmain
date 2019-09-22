85d9842f852e8c5bc6f5de90141409a25da964c8
garvit jain <garvitjain4333@gmail.com>
2019-09-23 02:28:52 +0530

    done
    login page and calander(50%)

--- app/src/main/java/com/aarush/gmain/HomePageActivity.java
+++ app/src/main/java/com/aarush/gmain/HomePageActivity.java
@@ -1,11 +1,46 @@
 package com.aarush.gmain;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-public class HomePageActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+
+public class HomePageActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonNewRegistrationForm;
+    private Button buttonEnquiryForm;
+    private Button buttonAccountManagement;
+    private Button buttonCalenderManagement;
+    private Button buttonMembershipManagement;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        buttonNewRegistrationForm = (Button) findViewById(R.id.buttonNewRegistrationForm);
+        buttonEnquiryForm = (Button) findViewById(R.id.buttonNewEnquiryForm);
+        buttonAccountManagement = (Button) findViewById(R.id.buttonAccountManagement);
+        buttonCalenderManagement = (Button) findViewById(R.id.buttonCalenderManagement);
+        buttonMembershipManagement = (Button) findViewById(R.id.buttonMembershipManagement);
+        findViewById(R.id.buttonCalenderManagement).setOnClickListener(this);
+        findViewById(R.id.buttonNewEnquiryForm).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonNewRegistrationForm:
+
+                break;
+            case R.id.buttonCalenderManagement:
+                startActivity(new Intent(this,Calendermap.class));
+                break;
+            case R.id.buttonNewEnquiryForm:
+                startActivity(new Intent(this,enquiryform.class));
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/aarush/gmain/enquiryform.java
@@ -0,0 +1,14 @@
+package com.aarush.gmain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class enquiryform extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enquiryform);
+    }
+}
